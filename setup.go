401fad4d8e904eb8ffcbee1ca9083d2c9e36876a
Aaron <44198148+whalehub@users.noreply.github.com>
2019-07-09 17:00:55 +0200

    Update the import paths for Caddy
    Signed-off-by: Aaron <github@jadja.eu>

--- setup.go
+++ setup.go
@@ -7,8 +7,8 @@ import (
 	"time"
 
 	"github.com/jstotz/arp"
-	"github.com/mholt/caddy"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 )
 
 type Arp struct {
